3e534046a602832ac36c337a1de938e97dfeb510
LamaEats <enemyworld@gmail.com>
2016-06-25 01:30:44 +0400

    1.1

--- js/app.js
+++ js/app.js
@@ -1,23 +1,10 @@
-var map;
-var center = {lat: 55.7471529, lng: 37.6441475};
-var options = {
-    center: center,
-    zoom: 14,
-    mapTypeControl: false,
-    streetViewControl: false,
-    draggable: false,
-    zoomControl: false,
-    scaleControl: false,
-    scrollwheel: false,
-    disableDoubleClickZoom: true
-};
-var pin = "images/pin.png"
-function initMap() {
-    map = new google.maps.Map(document.getElementById('map'), options);
-    var marker = new google.maps.Marker ({
-        position: center,
-        map: map,
-        title: "Our placement",
-        icon: pin
-    });
-}
+
+$('.l-item').on('click', function (event) {
+    event.preventDefault();
+    if (!$(this).hasClass('act')) {
+        $('.l-item').removeClass('act');
+        $(this).addClass('act');
+    } else {
+        $(this).removeClass('act');
+    }
+});
